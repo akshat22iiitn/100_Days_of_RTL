b6f8523d4c553eb487bb3da1687962625dbadfcf
akshat22iiitn <136363428+akshat22iiitn@users.noreply.github.com>
2024-09-08 11:10:41 +0530

    Create testbench.v

---
+++ Day008-HalfSubtractor & Full subtractor/testbench.v
@@ -0,0 +1,20 @@
+`timescale 1ns / 1ps
+
+
+
+module testbench();
+reg a,b,bin;
+wire diff,bout;
+full_subtractor uut(a,b,bin,diff,bout);
+initial begin
+a=0;b=0;bin=0; #10
+a=0;b=0;bin=1; #10
+a=0;b=1;bin=0; #10
+a=0;b=1;bin=1; #10
+a=1;b=0;bin=0; #10
+a=1;b=0;bin=1; #10
+a=1;b=1;bin=0; #10
+a=1;b=1;bin=1; #10
+$finish;
+end 
+endmodule
