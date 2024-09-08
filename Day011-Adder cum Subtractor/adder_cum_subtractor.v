b4b218df549e93cdcf11b97608cff0b66f5681f6
akshat22iiitn <136363428+akshat22iiitn@users.noreply.github.com>
2024-09-08 14:55:19 +0530

    Update adder_cum_subtractor.v

--- Day011-Adder cum Subtractor/adder_cum_subtractor.v
+++ Day011-Adder cum Subtractor/adder_cum_subtractor.v
@@ -10,8 +10,8 @@ output [3:0] sum;
 output cbout;
 wire [2:0] c;
  full_adder fa1(A[0],(B[0]^en),en,sum[0],c[0]);
- full_adder fa2(A[1],(B[1]^en),c[0],sum[0],c[0]);
- full_adder fa3(A[2],(B[2]^en),c[1],sum[0],c[0]);
- full_adder fa4(A[3],(B[3]^en),c[2],sum[0],cbout);
+ full_adder fa2(A[1],(B[1]^en),c[0],sum[1],c[1]);
+ full_adder fa3(A[2],(B[2]^en),c[1],sum[2],c[2]);
+ full_adder fa4(A[3],(B[3]^en),c[2],sum[3],cbout);
  
 endmodule
