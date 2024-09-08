dfa6fe8d3b31feb380b98fcdcfa1cf90d83fe48e
akshat22iiitn <136363428+akshat22iiitn@users.noreply.github.com>
2024-09-08 22:05:24 +0530

    Create decoder_3x8.v

---
+++ Day031-Decoder3x8/decoder_3x8.v
@@ -0,0 +1,23 @@
+`timescale 1ns / 1ps
+
+
+
+module Decoder_3x8(a,out);
+input [2:0] a; 
+output reg [7:0] out;
+ always@(*)
+ begin
+  out=8'b00000000;
+  case(a)
+  3'b000: out[0]=1'b1;
+  3'b001: out[1]=1'b1;
+  3'b010: out[2]=1'b1;
+  3'b011: out[3]=1'b1;
+  3'b100: out[4]=1'b1;
+  3'b101: out[5]=1'b1;
+  3'b110: out[6]=1'b1;
+   3'b111: out[7]=1'b1;
+   default: out=0;
+   endcase 
+   end
+endmodule
