b4c9c6e27cc60070c07d2fd03163f0202e88a41d
akshat22iiitn <136363428+akshat22iiitn@users.noreply.github.com>
2024-09-11 19:06:17 +0530

    Create testbench.v

---
+++ Day038- Gray to Binary/TestBench/testbench.v
@@ -0,0 +1,19 @@
+`timescale 1ns / 1ps
+
+
+
+module testbench;
+reg [3:0] g;
+wire [3:0] a; 
+ gray_to_binary uut(g,a);
+ initial begin 
+ g=4'b0000;#10
+ g=4'b0001;#10
+ g=4'b0100;#10
+ g=4'b0101;#10
+ g=4'b1111;#10
+ g=4'b1001;#10
+ g=4'b0110;#10
+ $finish;
+ end 
+endmodule
