c81fe8ab2a795c3a3de38c901d8b51a3d1a94a02
akshat22iiitn <136363428+akshat22iiitn@users.noreply.github.com>
2024-09-12 20:10:45 +0530

    Create siso.v

---
+++ Day056-Serial In Serial Out(SISO) Register/siso.v
@@ -0,0 +1,16 @@
+`timescale 1ns / 1ps
+//using behavioral modelling
+
+
+module serial(d_in,reset,clk,d_out);
+input d_in,reset,clk;
+output reg [3:0]  d_out;
+initial 
+d_out=4'b0000;
+always@(posedge clk) 
+ if(!reset)
+ d_out=4'b0000;
+ else  
+   d_out={d_in,d_out[3:1]};
+
+endmodule
