cfb2faf1d2e0843242dd6c6936bf3d6ac531e124
akshat22iiitn <136363428+akshat22iiitn@users.noreply.github.com>
2024-09-14 14:36:20 +0530

    Create square_gen.v

---
+++ Day035- N bit Number Square/square_gen.v
@@ -0,0 +1,16 @@
+`timescale 1ns / 1ps
+
+
+
+module square_gen(data,square,clk);
+parameter n=3;
+input [n-1:0] data;
+input clk;
+output reg [2*n-1:0] square;
+reg [2*n-1:0] temp;
+always@(posedge clk) 
+begin 
+     temp=data*data;
+     square=temp;
+ end 
+endmodule
