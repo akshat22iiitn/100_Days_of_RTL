3929e25455be1e4f352694d40bd7faf8b0bb8b6b
akshat22iiitn <136363428+akshat22iiitn@users.noreply.github.com>
2024-09-17 18:53:59 +0530

    Create testbench.v

---
+++ Day050- T Flip Flop/testbench.v
@@ -0,0 +1,19 @@
+`timescale 1ns / 1ps
+
+
+
+
+module testbench;
+reg t,clk,reset;
+wire q; 
+T_flipflop uut(t,clk,reset,q);
+always #5 clk=~clk;
+initial begin 
+          clk=1;
+        reset=0; t=0; #10 
+        reset=0; t=1; #10
+        reset=1; t=0; #10
+        reset=1; t=1; #10
+ $finish;
+ end 
+endmodule
