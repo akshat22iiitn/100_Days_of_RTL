ed34df0c0bcfe81fae41d5e6d21bad77dfe3ade8
akshat22iiitn <136363428+akshat22iiitn@users.noreply.github.com>
2024-09-22 18:21:56 +0530

    Update SR_latch.v

--- Day047- SR Latch/SR_latch.v
+++ Day047- SR Latch/SR_latch.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 05.09.2024 15:35:46
-// Design Name: 
-// Module Name: SR_latch
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+/////
 
 
 module SR_latch(s,r,en,q,qb);
