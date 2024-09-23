bbc8e811640620e9a5c9689df17134b162029d0c
akshat22iiitn <136363428+akshat22iiitn@users.noreply.github.com>
2024-09-23 22:34:58 +0530

    Update testbench.v

--- Day016- N Bit Divider/testbench.v
+++ Day016- N Bit Divider/testbench.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 23.09.2024 22:15:18
-// Design Name: 
-// Module Name: testbench
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+
 
 
 module testbench;
