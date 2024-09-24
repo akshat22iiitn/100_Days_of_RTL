0e67ee55f2df8132f44053a3b5aab4005ea71bf9
akshat22iiitn <136363428+akshat22iiitn@users.noreply.github.com>
2024-09-24 21:32:21 +0530

    Create jk_flip_flop.v

---
+++ Day051- SR FlipFlop using JK,T and D FF/jk_flip_flop.v
@@ -0,0 +1,36 @@
+`timescale 1ns / 1ps
+
+
+module jk_flip_flop(j,k,reset,clk,q,qb);
+input j,k,clk,reset;
+output reg q,qb;
+always@(posedge clk) begin  
+if(reset) begin 
+    q=1'b0;
+    qb=1'b0;
+end 
+else begin
+if(j==0 && k==0)
+  begin
+    q=(q);
+    qb=(~q);
+  end 
+if(j==1 && k==0) 
+ begin
+    q=j;
+    qb=k;
+ end 
+if(j==0 && k==1) 
+            begin
+              q=j;
+              qb=k;
+            end 
+  
+if(j==1 && k==1) 
+            begin
+    q=(~q);
+    qb=(~qb);
+            end 
+    end 
+end 
+endmodule
