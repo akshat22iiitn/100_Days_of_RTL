f6404bf294517efa1b1aa1be370d03cff06346cb
akshat22iiitn <136363428+akshat22iiitn@users.noreply.github.com>
2024-09-25 19:36:16 +0530

    Create T_flipflop.v

---
+++ Day052- JK Flip Flop using SR,T and D FlipFlop/T_flipflop.v
@@ -0,0 +1,19 @@
+`timescale 1ns / 1ps
+
+
+
+module T_flipflop(t,clk,reset,q);
+input t,clk,reset;
+output reg q;
+always@(posedge clk) 
+  begin
+  if(reset) 
+  q=1'b0;
+  else begin 
+        if(t) 
+            q=~q;
+        else 
+             q=t;
+        end 
+ end  
+endmodule
