10bc8061c5b5c3e6b5926dc5c99de6688b553ef4
akshat22iiitn <136363428+akshat22iiitn@users.noreply.github.com>
2024-09-26 16:43:09 +0530

    Create conversion_d.v

---
+++ Day053- D FlipFlop using SR,T and JK FlipFlop/conversion_d.v
@@ -0,0 +1,20 @@
+`timescale 1ns / 1ps
+
+
+module conversion_d(qsr,qt,qjk,clk,reset,d); 
+input clk,reset,d;
+output qsr,qt,qjk;
+wire w1,w2,w3;
+
+SR_flipflop sr(d,~d,clk,qsr,~qsr);  
+
+assign w1=d&(~qt);
+assign w2=(~d)&qt;
+assign w3=w1|w2; 
+
+T_flipflop tb(w3,clk,reset,qt); 
+
+jk_flip_flop jk(d,~d,reset,clk,qjk,~qjk);
+
+endmodule
+
