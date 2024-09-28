e8d6792ca2e8e8bd71f6a22af88d523b93b78ac5
akshat22iiitn <136363428+akshat22iiitn@users.noreply.github.com>
2024-09-28 19:57:11 +0530

    Update testbench.v

--- Day050- T Flip Flop/testbench.v
+++ Day050- T Flip Flop/testbench.v
@@ -10,7 +10,7 @@ T_flipflop uut(t,clk,reset,q);
 always #5 clk=~clk;
 initial begin 
           clk=1;
-        reset=0; t=0;  #10 
+        reset=0; t=0; #10      
         reset=0; t=1; #10
         reset=1; t=0; #10
         reset=1; t=1; #10
