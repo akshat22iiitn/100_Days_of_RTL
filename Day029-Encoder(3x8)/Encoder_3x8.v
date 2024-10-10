d7e56ef1e02134ec7a0b41d803ded30f3536be61
akshat22iiitn <136363428+akshat22iiitn@users.noreply.github.com>
2024-10-10 18:20:08 +0530

    Update Encoder_3x8.v

--- Day029-Encoder(3x8)/Encoder_3x8.v
+++ Day029-Encoder(3x8)/Encoder_3x8.v
@@ -4,6 +4,6 @@ module Encoder_3x8(d,a,b,c);
 input [7:0] d;
 output a,b,c;
   assign a= d[4]+d[5]+d[6]+d[7                  ];
-assign b= d[2]+d[3]+d[6]+d[7];
+  assign b= d[2]+d[3]+d[6]+d[7];         
 assign c= d[1]+d[3]+d[5]+d[7];
 endmodule
