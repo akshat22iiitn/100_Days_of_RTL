c879552172dff495f3c9ee2bacd5a0406f86f8ec
akshat22iiitn <136363428+akshat22iiitn@users.noreply.github.com>
2024-10-11 21:00:53 +0530

    Update binary_to_2s_complement.v

--- Day039- Binary to Two's Complement/binary_to_2s_complement.v
+++ Day039- Binary to Two's Complement/binary_to_2s_complement.v
@@ -6,6 +6,6 @@ module binary_to_2s_complement(data,out);
 input [3:0] data;
 output [3:0] out; 
 wire [3:0] temp; 
-    assign temp=(~dat     a);
+    assign temp=(~dat      a);
     assign out=temp+4'b0001;
 endmodule
