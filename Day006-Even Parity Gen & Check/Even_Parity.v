bd4c6ec53a52a2de81cbe52f227d7ac4fe32c05b
akshat22iiitn <136363428+akshat22iiitn@users.noreply.github.com>
2024-10-15 21:49:28 +0530

    Update Even_Parity.v

--- Day006-Even Parity Gen & Check/Even_Parity.v
+++ Day006-Even Parity Gen & Check/Even_Parity.v
@@ -1,7 +1,7 @@
 `timescale 1ns / 1ps
 
 module Even_Parity(b,p);
-input [2:0]b;
+  input [2:0]b; 
 output  p; 
 assign
   p=b[2]&&(~b[1])&&(~b[0]) || (~b[1])&&b[0]&&(~b[2]) || (b[2]&&b[1]&&b[0]) || (~b[2])&&b[1]&&(~b[0]);   
