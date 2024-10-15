7de53a527abebb2fdb22731af416e2363fa32288
akshat22iiitn <136363428+akshat22iiitn@users.noreply.github.com>
2024-10-15 21:49:21 +0530

    Update Even_Parity.v

--- Day006-Even Parity Gen & Check/Even_Parity.v
+++ Day006-Even Parity Gen & Check/Even_Parity.v
@@ -2,7 +2,7 @@
 
 module Even_Parity(b,p);
 input [2:0]b;
-output  p;
+output  p; 
 assign
   p=b[2]&&(~b[1])&&(~b[0]) || (~b[1])&&b[0]&&(~b[2]) || (b[2]&&b[1]&&b[0]) || (~b[2])&&b[1]&&(~b[0]);   
 endmodule
