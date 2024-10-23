f3bbea25c48cc43e49c7c467855c1a559f16b376
akshat22iiitn <136363428+akshat22iiitn@users.noreply.github.com>
2024-10-23 17:12:44 +0530

    Update binary_to_gray.v

--- Day037-Binary to Gray Convertor/binary_to_gray.v
+++ Day037-Binary to Gray Convertor/binary_to_gray.v
@@ -4,7 +4,7 @@ module binary_to_gray(a,g);
 input [3:0] a;
 output [3:0] g;
     assign g[3]=a[3];
-    assign g[2]=a[3]^a[2]; 
+    assign g[2]=a[3]^a[2];  
     assign g[1]=a[2]^a[1];
     assign g[0]=a[1]^a[0];  
 endmodule
