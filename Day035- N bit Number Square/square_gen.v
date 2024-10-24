ce8c23950b92dd748345040631b94d44f509f442
akshat22iiitn <136363428+akshat22iiitn@users.noreply.github.com>
2024-10-24 19:27:27 +0530

    Update square_gen.v

--- Day035- N bit Number Square/square_gen.v
+++ Day035- N bit Number Square/square_gen.v
@@ -6,7 +6,7 @@ module square_gen(data,square,clk);
 parameter n=3;       
 input [n-1:0] data;
 input clk;
-output reg [2*n-1:0] square;
+     output reg [2*n-1:0] square; 
 reg [2*n-1:0] temp;
 always@(posedge clk) 
 begin 
